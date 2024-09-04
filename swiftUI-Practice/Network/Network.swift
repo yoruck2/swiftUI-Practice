f021b54cbefa2942e54ffe585625723c6e424864
yoruck2 <yoruck2@gmail.com>
2024-09-05 02:40:41 +0900

    ✨ CoinAPI 와 모델 구현

--- swiftUI-Practice/Network/Network.swift
+++ swiftUI-Practice/Network/Network.swift
@@ -7,12 +7,47 @@
 
 import SwiftUI
 
-struct Network: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+typealias Markets = [Market]
+
+struct CoinAPI {
+    
+    private init() { }
+
+    static func fetchMarketGCD(completion: @escaping (Markets) -> Void) {
+        
+        let url = URL(string: "https://api.upbit.com/v1/market/all")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data else { return }
+            do {
+                let decodedData = try JSONDecoder().decode(Markets.self, from: data)
+                DispatchQueue.main.async {
+                    completion(decodedData)
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    static func fetchMarketConcurreny() async throws -> Markets {
+    
+//        let url = URL(string: "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=1,2,3,4,5")!
+        let url = URL(string: "https://api.upbit.com/v1/market/all")!
+        var request = URLRequest(url: url)    
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        let decodedData = try JSONDecoder().decode(Markets.self, from: data)
+        return decodedData
     }
 }
 
-#Preview {
-    Network()
+struct Market: Hashable, Codable, Identifiable {
+    var id = UUID()
+    let market, koreanName, englishName: String
+
+    enum CodingKeys: String, CodingKey {
+        case market
+        case koreanName = "korean_name"
+        case englishName = "english_name"
+    }
 }
